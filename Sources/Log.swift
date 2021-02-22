7bdb611e684f9fcc6fa76722e49072b527646cdc
junhyi.park <junhyi.park@gmail.com>
2021-02-23 00:46:26 +0900

    spm 설정 init

---
+++ Sources/Log.swift
@@ -0,0 +1,14 @@
+//
+//  Log.swift
+//  TLLogger
+//
+//  Created by tilltue on 2021/02/23.
+//
+
+import Foundation
+
+public class Log {
+    public init() {
+        
+    }
+}
---
+++ TLLogerTest/TLLogerTests/TLLogerTests.swift
@@ -0,0 +1,20 @@
+//
+//  TLLogerTestTests.swift
+//  TLLogerTestTests
+//
+//  Created by tilltue on 2022/01/11.
+//
+
+import XCTest
+@testable import TLLogger
+
+class TLLogerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
