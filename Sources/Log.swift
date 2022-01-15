c3d37647dff2b19c8ff9caac5d99be44992b65b7
tilltue <junhyi.park@gmail.com>
2022-01-15 23:44:02 +0900

    로그 레벨 테스트 작성 및 구현

--- Sources/Log.swift
+++ Sources/Log.swift
@@ -7,8 +7,39 @@
 
 import Foundation
 
-public class Log {
+extension Log {
+    public enum Level {
+        case verbose
+        case debug
+        case info
+        case warning
+        case error
+    }
+    
+    public struct LogData {
+        public let level: Level
+        public let message: String
+        
+        init(_ level: Level, _ message: String) {
+            self.level = level
+            self.message = message
+        }
+    }
+}
+
+public final class Log {
+    
+    private var logData: [LogData] = []
+    
     public init() {
         
+    }
+    
+    public func log(_ level: Level, _ message: String) {
+        logData.insert(.init(level, message), at: 0)
+    }
+    
+    public var dataSources: [LogData] {
+        return logData
     }
 }
--- TLLogerTest/TLLogerTests/TLLogerTests.swift
+++ TLLogerTest/TLLogerTests/TLLogerTests.swift
@@ -17,4 +17,32 @@ class TLLogerTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testLogDataSources() {
+        // Arrange
+        let sut = Log()
+        
+        // Act
+        sut.log(.verbose, "verbose level log")
+        sut.log(.debug, "debug level log")
+        sut.log(.info, "info level log")
+        sut.log(.warning, "warning level log")
+        sut.log(.error, "error level log")
+        
+        // Assert
+        XCTAssertEqual(sut.dataSources.count, 5)
+        XCTAssertEqual(sut.dataSources,
+                       [.init(.error, "error level log"),
+                        .init(.warning, "warning level log"),
+                        .init(.info, "info level log"),
+                        .init(.debug, "debug level log"),
+                        .init(.verbose, "verbose level log")
+                       ])
+    }
+}
+
+extension Log.LogData: Equatable {
+    public static func==(lhs: Self, rhs: Self) -> Bool {
+        return lhs.level == rhs.level && lhs.message == rhs.message
+    }
 }
